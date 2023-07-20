7b7a81fa062f1ddfe31d4c925a3f2f4e5e720529
Deepayan-Bose <108746346+Deepayan-Bose@users.noreply.github.com>
2023-07-20 11:23:00 +0530

    Update 3_sum(two_pointer_approach).cpp

--- Array/3_sum(two_pointer_approach).cpp
+++ Array/3_sum(two_pointer_approach).cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        /*
         //Two pointer approach
         vector<vector<int>> ans;
         int n=nums.size();
@@ -28,6 +29,29 @@ public:
         }
         
         return ans;
+        */
+        //Hashmap approach
+        sort(nums.begin(),nums.end());
+        if(nums[0]>0) return {};
+        if(nums.size()<3) return {};
+        vector<vector<int>> ans;
+        unordered_map<int,int> mpp;
+
+        int n=nums.size();
+        for(int i=0;i<n;i++) mpp[nums[i]]=i;
+        for(int i=0;i<n-2;i++){
+            if(nums[i]>0) break;
+            for(int j=i+1;j<n;j++){
+                int required=-1*(nums[i]+nums[j]);
+                if(mpp.find(required)!=mpp.end() and mpp.find(required)->second>j){
+                    ans.push_back({nums[i],nums[j],required});
+                }
+                j=mpp.find(nums[j])->second;
+            }
+            i=mpp.find(nums[i])->second;
+        }
         
+        return ans;
+
     }
 };
