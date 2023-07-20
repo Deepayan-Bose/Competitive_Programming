4982becc13963b8c3477d22b6619d8d1b2451aed
Deepayan-Bose <108746346+Deepayan-Bose@users.noreply.github.com>
2023-07-20 10:53:05 +0530

    Create 3_sum(two_pointer_approach).cpp

---
+++ Array/3_sum(two_pointer_approach).cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        //Two pointer approach
+        vector<vector<int>> ans;
+        int n=nums.size();
+        int j,k;
+        sort(nums.begin(),nums.end());
+        if(nums.size()<3) return {};
+        if(nums[0]>0) return {}; // If first element in sorted array is positive then no such triplet exists such that their sum is zero
+        for(int i=0;i<n-2;i++){
+            if(nums[i]>0) break;
+            if(i!=0 and nums[i]==nums[i-1]) continue; // To avoid duplication
+            j=i+1;
+            k=n-1;
+            while(j<k){
+                if(nums[j]+nums[k]<-nums[i]) j++;
+                else if(nums[j]+nums[k]>-nums[i]) k--;
+                else {
+                    vector<int> anspart={nums[i],nums[j],nums[k]};
+                    //sort(anspart.begin(),anspart.end());
+                    ans.push_back(anspart);
+                    int last_low_occurence=nums[j],last_high_occurence=nums[k];
+                    while(j<k and nums[j]==last_low_occurence) j++;
+                    while(k> j and nums[k]==last_high_occurence) k--;
+                }
+            }
+        }
+        
+        return ans;
+        
+    }
+};
