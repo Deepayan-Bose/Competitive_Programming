1587f37bffe0192816b12127446ffad1f03fa54d
Deepayan-Bose <108746346+Deepayan-Bose@users.noreply.github.com>
2024-02-23 11:28:19 +0530

    Add files via upload

--- assign2_1.java
+++ assign2_1.java
@@ -1,88 +1,188 @@
 import java.util.*;
 
-class Customer {
+class Customer{
 	int customerId;
 	String name;
 	double loanAmount;
 	String phoneNumber;
 	static double creditLimit=80000;
-	
-	Customer(int customerId,String name,double loanAmount,String phoneNumber){
-		this.customerId=customerId;
+
+	Customer(String name,String phoneNumber){
+		customerId=(int)(Math.random()*100);
 		this.name=String.valueOf(name);
-		this.loanAmount=loanAmount;
 		this.phoneNumber=String.valueOf(phoneNumber);
-	}/*
-	public static void setCreditLimit(double newCreditLimit){
-		creditLimit=newCreditLimit;
-		System.out.println("The credit limit has been set");
-	}*/
+		loanAmount=0;
+	}
+	Customer(){}
+	public void changeName(String name){
+		this.name=String.valueOf(name);
+		System.out.println("The name has been updated");
+	}
 	
-	public void grantLoan(double requestAmount){
-		if(loanAmount+requestAmount>creditLimit){
-			System.out.println("The loan cannot be granted as it crosses the credit limit");
-		}
-		else{
-			loanAmount=loanAmount+requestAmount;
-			System.out.println("The loan has been granted to the account holder");
-		}
+	public void changePhoneNumber(String phoneNumber){
+		this.phoneNumber=String.valueOf(phoneNumber);
+		System.out.println("The phone number has been updated");
 	}
+
 	public void showCreditLimit(){
-		System.out.println("The credit limit for this account is "+creditLimit);
+		System.out.println("The credit limit of the user is : "+creditLimit);
 	}
-	public void showLoanAmount(){
-		System.out.println("The loan amount for this account is "+loanAmount);
+	static void setCreditLimit(double newCreditLimit){
+		creditLimit=newCreditLimit;
+		System.out.println("The credit limit has been updated ");
 	}
-	public void showMaxSeekAmount(){
-		System.out.println("The amount of loan the user can seek is "+(creditLimit-loanAmount)+"INR");
+	public void checkLoanAmount(){
+		System.out.println("The loan amount of the user is : "+loanAmount);
 	}
-	public Boolean checkCustomerId(int customerId){
-		return (this.customerId==customerId);
+	public void checkSeekingAmount(){
+		System.out.println("The loan he can be granted further is : "+ (creditLimit-loanAmount));
 	}
+	public void grantLoan(double loanMoney){
+		if((loanAmount+loanMoney)>creditLimit) {
+			System.out.println("The loan cannot be granted as it exceeds the credit limit");
+		}else{
+			loanAmount+=loanMoney;
+			System.out.println("The loan has been sanctioned to the user");
+		}
+	}
+	public void displayAttributes(){
+		System.out.print("Name : "+name+"\nPhone Number : " +phoneNumber+"\nLoan Amount : "+loanAmount+"\nCustomer Id : "+customerId+"\n");
+	}
+
+	
 }
 
-class PriviledgeCustomer extends Customer{
+class PriviledgedCustomer extends Customer{
 	static double creditLimit=100000;
-
-	
-	PriviledgeCustomer(int customerId,String name,double loanAmount,String phoneNumber){
-		super(customerId,name,loanAmount,phoneNumber);
+	PriviledgedCustomer(String name,String phoneNumber){
+		super(name,phoneNumber);
 	}
+	PriviledgedCustomer(){}
 	public void showCreditLimit(){
-                System.out.println("The credit limit for this account is "+creditLimit);
+                System.out.println("The credit limit of the user is : "+creditLimit);
         }
-	public void showMaxSeekAmount(){
-                System.out.println("The amount of loan the user can seek is "+(creditLimit-loanAmount)+"INR");
+        static void setCreditLimit(double newCreditLimit){
+                creditLimit=newCreditLimit;
+                System.out.println("The credit limit has been updated ");
         }
-	public void grantLoan(double requestAmount){
-                if(loanAmount+requestAmount>creditLimit){
-                        System.out.println("The loan cannot be granted as it crosses the credit limit");
+	public void checkSeekingAmount(){
+                System.out.println("The loan he can be granted further is : "+ (creditLimit-loanAmount));
+        }
+        public void grantLoan(double loanMoney){
+                if((loanAmount+loanMoney)>creditLimit) {
+                        System.out.println("The loan cannot be granted as it exceeds the credit limit");
+                }else{
+                        loanAmount+=loanMoney;
+                        System.out.println("The loan has been sanctioned to the user");
                 }
-                else{
-                        loanAmount=loanAmount+requestAmount;
-                        System.out.println("The loan has been granted to the account holder");
-                }
         }
-
 }
 
 class Bank{
 	public static void main(String[] args){
-		Scanner myScanner = new Scanner(System.in);
-		Customer customer = new Customer(123,"Rajesh",0,"9024372134");
-		PriviledgeCustomer priviledgeCustomer = new PriviledgeCustomer(124,"Suresh",0,"7829437223");
-		//PriviledgeCustomer.setCreditLimit(100000);
-		//Customer.setCreditLimit(80000);
-		customer.showCreditLimit();
-		priviledgeCustomer.showCreditLimit();
-		customer.grantLoan(40000.34);
-		priviledgeCustomer.grantLoan(50000.67);
-		customer.showLoanAmount();
-		priviledgeCustomer.showLoanAmount();
-		customer.showMaxSeekAmount();
-		priviledgeCustomer.showMaxSeekAmount();
-		customer.grantLoan(40000);
-		priviledgeCustomer.grantLoan(40000);
+		String name,phoneNumber;
+		int option;
+		Customer customer;
+		PriviledgedCustomer priviledgedCustomer;
+		Scanner myObj = new Scanner (System.in);
+		System.out.println("Enter the details of the customer ");
+		System.out.print("Name : ");
+		name  = myObj.nextLine();
+		System.out.print("Phone Number : ");
+		phoneNumber = myObj.nextLine();
+		
+		System.out.print("Is the cutomer :\n1.Priviledged\n2.Unpriviledged\n");
+		option = myObj.nextInt();
+		switch(option){
+			case 1:
+				priviledgedCustomer= new PriviledgedCustomer(name,phoneNumber);
+				while(true){
+					System.out.print("What would you like to do\n1.Grant a loan\n2.Change the name\n3.Change the phone number\n4.Check loan amount\n5.Check credit limit\n6.Check loan that the user can seek\n7.Display the details of the user\n8.Exit\n");
+					int option2=myObj.nextInt();
+					if(option2==8) break;
+					switch(option2){
+						case 1:
+							System.out.println("Enter the amount to sanction");
+							double loanMoney= myObj.nextDouble();
+							priviledgedCustomer.grantLoan(loanMoney);
+							break;
+						case 2:
+							System.out.println("Enter the new name");
+							myObj.nextLine();
+							String newName = myObj.nextLine();
+							priviledgedCustomer.changeName(newName);
+							break;
+						case 3:
+							System.out.println("Enter the new phone number");
+							myObj.nextLine();
+							String newPhoneNumber= myObj.nextLine();
+							priviledgedCustomer.changePhoneNumber(newPhoneNumber);
+							break;
+						case 4:
+							priviledgedCustomer.checkLoanAmount();
+							break;
+						case 5:
+							priviledgedCustomer.showCreditLimit();
+							break;
+						case 6: 
+							priviledgedCustomer.checkSeekingAmount();
+							break;
+						case 7:
+							priviledgedCustomer.displayAttributes();
+							break;
+						default:
+							System.out.println("Please enter a valid option");
+							break;
+					}
+				}
+				break;
+			case 2:
+				customer= new Customer(name,phoneNumber);
+				while(true){
+                                        System.out.print("What would you like to do\n1.Grant a loan\n2.Change the name\n3.Change the phone number\n4.Check loan amount\n5.Check credit limit\n6.Check loan that the user can seek\n7.Display the details of the user\n8.Exit\n");
+                                        int option2=myObj.nextInt();
+                                        if(option2==8) break;
+                                        switch(option2){
+                                                case 1:
+                                                        System.out.println("Enter the amount to sanction");
+                                                        double loanMoney= myObj.nextDouble();
+                                                        customer.grantLoan(loanMoney);
+                                                        break;
+                                                case 2:
+                                                        System.out.println("Enter the new name");
+							myObj.nextLine();
+                                                        String newName = myObj.nextLine();
+                                                        customer.changeName(newName);
+                                                        break;
+                                                case 3:
+                                                        System.out.println("Enter the new phone number");
+							myObj.nextLine();
+                                                        String newPhoneNumber= myObj.nextLine();
+                                                        customer.changePhoneNumber(newPhoneNumber);
+                                                        break;
+                                                case 4:
+                                                        customer.checkLoanAmount();
+                                                        break;
+                                                case 5:
+                                                        customer.showCreditLimit();
+                                                        break;
+                                                case 6:
+                                                        customer.checkSeekingAmount();
+                                                        break;
+                                                case 7:
+                                                        customer.displayAttributes();
+                                                        break;
+                                                default:
+                                                        System.out.println("Please enter a valid option");
+                                                        break;
+                                        }               
+                                }
+				break;
+			default:
+				System.out.println("Not a valid option, aborting ...");
+				break;
+
+		}
+
 	}
 }
-
--- assign2_2.java
+++ assign2_2.java
@@ -1,91 +1,310 @@
 import java.util.*;
 
-class Address{
-	int premiseNumber;
-	String street;
-	String city;
-	int pin;
-	String state;
-	Address(int premiseNumber,String street,String city){
-		this.premiseNumber=premiseNumber;
-		this.street=String.valueOf(street);
-		this.city=String.valueOf(city);
-	}
-	
-	public void setPremiseNumber(int premiseNumber){
-		this.premiseNumber=premiseNumber;
-	}
-	public void setStreet(String street){
-		this.street=String.valueOf(street);
-	}
-	public void setCity(String city){
-		this.city=String.valueOf(city);
-	}
-	public void setPin(int pin){
-		this.pin=pin;
-	}
-	public void setState(String state){
-		this.state=String.valueOf(state);
-	}
-	public void displayAttributes(){
-		System.out.print("Address:\nPremise Number : "+premiseNumber + "\nStreet : " + street + "\nCity : "+city+"\nPin : "+ pin +"\nState : "+state +"\n");
-	}
-
-}
 class Person{
-	String name;
-	Address address;
-	String phoneNumber;
-	String emailId;
-	//constructor
-	Person(String name,Address address,String phoneNumber,String emailId){
-		this.name=String.valueOf(name);
-		this.address=new Address(address);
-		this.phoneNumber=String.valueOf(phoneNumber);
-		this.emailId=String.valueOf(emailId);
-	}
-	//Setters
-	public void setName(String name){
-		this.name=String.valueOf(name);
-	}
-	public void setAddress(Address address){
-		Scanner myScanner= new Scanner(System.in);
-		System.out.println("What would you like to change :\n1.PremiseNumber\n2.Street\n3.City\n4.Pin\n5.State");
-		int option=myScanner.nextInt();
-		switch(option){
-			case 1:
-				System.out.println("Enter the new premise number : ");
-				int newPremiseNumber=myScanner.nextInt();
-				address.setPremiseNumber(newPremiseNumber);
-				break;
-			case 2:
-				
-		}
-	}
-	public void setPhoneNumber(String phoneNumber){
-		this.phoneNumber=String.valueOf(phoneNumber);
-	}
-	public void setEmailId(String emailId){
-		this.emailId=String.valueOf(emailId);
-	}
-	//Getters
-	public void displayAttributes(){
-		System.out.println("Name : "+name);
-		address.displayAttributes();
-		System.out.print("PhoneNumber : "+phoneNumber+"\nEmailId : "+emailId+"\n");
-		
+    String name;
+    class Address{
+        int premiseNumber;
+        String street;
+        String city;
+        String pin;
+        String state;
 
-	}
+        Address(int premiseNumber,String street,String city,String pin,String state){
+            this.premiseNumber = premiseNumber;
+            this.street = String.valueOf(street);
+            this.city = String.valueOf(city);
+            this.pin = String.valueOf(pin);
+            this.state = String.valueOf(state);
+        }
+        public void setPremiseNumber(int premiseNumber){
+            this.premiseNumber=premiseNumber;
+        }
+        public void setStreet(String street){
+            this.street = String.valueOf(street);
+        }
+        public void setCity(String city){
+            this.city = String.valueOf(city);
+        }
+        public void setPin(String pin){
+            this.pin = String.valueOf(pin);
+        }
+        public void setState(String state){
+            this.state = String.valueOf(state);
+        }
 
+        public void displayAttributes(){
+            System.out.print("Premise Number : "+premiseNumber+"\nStreet : "+street+"\nCity : "+city+"\nPin : "+pin+"\nState : "+state +"\n");
+        }
+
+    }
+    Address address;
+    String phoneNumber;
+    String email;
+    Person(){}
+    Person(String name, int premiseNumber, String street, String city, String pin, String state, String phoneNumber, String email){
+        this.name = String.valueOf(name);
+        this.address = new Address(premiseNumber,street,city,pin,state);
+        this.phoneNumber = String.valueOf(phoneNumber);
+        this.email = String.valueOf(email);
+    }
+    public void displayAttributes(){
+        System.out.print("Name : "+name +"\n");
+        address.displayAttributes();
+        System.out.print("Phone Number : "+ phoneNumber +"\nEmail : "+email+"\n");
+    }
+    public void setName(String name){
+        this.name = String.valueOf(name);
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber = String.valueOf(phoneNumber);
+    }
+    public void setEmail(String email){
+        this.email = String.valueOf(email);
+    }
+    public void setAddress(String value,int option){
+        switch(option){
+            case 1:
+                address.setPremiseNumber(Integer.valueOf(value));
+                break;
+            case 2:
+                address.setStreet(value);
+                break;
+            case 3:
+                address.setCity(value);
+                break;
+            case 4:
+                address.setPin(value);
+                break;
+            case 5:
+                address.setState(value);
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 class Student extends Person{
-	static int lastRollNo;
-	int rollNumber;
-	String course;
+    int rollNumber;
+    String courseOfStudy;
+    Student(){super();}
+    Student(String name, int premiseNumber, String street, String city, String pin, String state, String phoneNumber, String email,int rollNumber,String courseOfStudy){
+        super(name,premiseNumber,street,city,pin,state,phoneNumber,email);
+        this.rollNumber = rollNumber;
+        this.courseOfStudy = String.valueOf(courseOfStudy);
+    }
+    public void setRollNumber(int rollNumber){
+        this.rollNumber=rollNumber;
+    }
+    public void setCourse(String courseOfStudy){
+        this.courseOfStudy = String.valueOf(courseOfStudy);
+    }
 
-	public void setCourse(String course){
-		this.course=String.valueOf(course);
-	}
+    public void displayAttributes(){
+        super.displayAttributes();
+        System.out.print("Roll Number : "+rollNumber+"\nCourse of Study : "+courseOfStudy+"\n");
+    }
+}
 
-	
+class Faculty extends Person{
+    int id;
+    String dept;
+    String specialization;
+    Faculty(){super();}
+    Faculty(String name, int premiseNumber, String street, String city, String pin, String state, String phoneNumber, String email,int id,String dept,String specialization){
+        super(name,premiseNumber,street,city,pin,state,phoneNumber,email);
+        this.id=id;
+        this.dept = String.valueOf(dept);
+        this.specialization = String.valueOf(specialization);
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setDepartment(String dept){
+        this.dept=String.valueOf(dept);
+    }
+    public void setSpecialization(String specialization){
+        this.specialization = String.valueOf(specialization);
+    }
+
+    public void displayAttributes(){
+        super.displayAttributes();
+        System.out.print("Id : "+id+"\nDepartment : "+dept+"\nSpecialization : "+specialization+"\n");
+    }
+}
+
+class Institute{
+    public static void main(String[] args){
+        Student student;
+        Faculty faculty;
+        String name,street,city,pin,state,phoneNumber,email,specialization,dept,courseOfStudy;
+        int premiseNumber,rollNumber,id;
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Enter the name : ");
+        name=myObj.nextLine();
+        
+        System.out.println("Enter the premise number : ");
+        premiseNumber = myObj.nextInt();
+        myObj.nextLine();
+        System.out.println("Enter the street : ");
+        street = myObj.nextLine();
+
+        System.out.println("Enter the city : ");
+        city = myObj.nextLine();
+
+        System.out.println("Enter the pin : ");
+        pin = myObj.nextLine();
+
+        System.out.println("Enter the state : ");
+        state = myObj.nextLine();
+
+        System.out.println("Enter the phone number : ");
+        phoneNumber = myObj.nextLine();
+
+        System.out.println("Enter the email id : ");
+        email = myObj.nextLine();
+
+        int option;
+
+        System.out.print("Is the person :\n1.Student\n2.Faculty\n");
+        option = myObj.nextInt();
+
+        switch(option){
+            case 1:
+                System.out.println("Enter the roll number : ");
+                rollNumber = myObj.nextInt();
+                myObj.nextLine();
+                System.out.println("Enter the course of study : ");
+                courseOfStudy = myObj.nextLine();
+                student = new Student(name,premiseNumber,street,city,pin,state,phoneNumber,email,rollNumber,courseOfStudy);
+                while(true){
+                    System.out.print("What would you like to do :\n1.Change the name\n2.Change the address\n3.Change the phone number\n4.Change the email\n5.Change the roll number\n6.Change the course of study\n7.Display student details\n8.Exit\n");
+                    int option2;
+                    option2 = myObj.nextInt();
+                    if(option2==8) break;
+                    switch(option2){
+                        case 1:
+                            myObj.nextLine();
+                            System.out.println("Enter the new name : ");
+                            name=myObj.nextLine();
+                            student.setName(name);
+                            break;
+                        case 2:
+                            int option3;
+                            String value;
+                            System.out.print("What would you like to change\n1.Premise Number \n2.Street\n3.City\n4.Pin\n5.State\n");
+                            option3 = myObj.nextInt();
+                            myObj.nextLine();
+                            System.out.println("Enter the new value : ");
+                            value = myObj.nextLine();
+                            student.setAddress(value,option3);
+                            break;
+                        case 3:
+                            myObj.nextLine();
+                            System.out.println("Enter the phone number : ");
+                            phoneNumber = myObj.nextLine();
+                            student.setPhoneNumber(phoneNumber);
+                            break;
+                        case 4:
+                            myObj.nextLine();
+                            System.out.println("Enter the email id : ");
+                            email= myObj.nextLine();
+                            student.setEmail(email);
+                            break;
+                        case 5:
+                            System.out.println("Enter the roll number : ");
+                            rollNumber = myObj.nextInt();
+                            student.setRollNumber(rollNumber);
+                            break;
+                        case 6:
+                            myObj.nextLine();
+                            System.out.println("Enter the course of study : ");
+                            courseOfStudy = myObj.nextLine();
+                            student.setCourse(courseOfStudy);
+                            break;
+                        case 7:
+                            student.displayAttributes();
+                            break;
+                        default :
+                            System.out.println("Not a valid option ...");
+                            break;
+                    }
+                }
+                break;
+            case 2:
+                System.out.println("Enter the id : ");
+                id = myObj.nextInt();
+                myObj.nextLine();
+                System.out.println("Enter the dept : ");
+                dept = myObj.nextLine();
+                System.out.println("Enter the specialization : ");
+                specialization = myObj.nextLine();
+                faculty = new Faculty(name,premiseNumber,street,city,pin,state,phoneNumber,email,id,dept,specialization);
+                while(true){
+                    System.out.print("What would you like to do :\n1.Change the name\n2.Change the address\n3.Change the phone number\n4.Change the email\n5.Change the id\n6.Change the dept\n7.Change the specialization\n8.Display student details\n9.Exit\n");
+                    int option2;
+                    option2 = myObj.nextInt();
+                    if(option2==9) break;
+                    switch(option2){
+                        case 1:
+                            myObj.nextLine();
+                            System.out.println("Enter the new name : ");
+                            name=myObj.nextLine();
+                            faculty.setName(name);
+                            break;
+                        case 2:
+                            int option3;
+                            String value;
+                            System.out.print("What would you like to change\n1.Premise Number \n2.Street\n3.City\n4.Pin\n5.State\n");
+                            option3 = myObj.nextInt();
+                            myObj.nextLine();
+                            System.out.println("Enter the new value : ");
+                            value = myObj.nextLine();
+                            faculty.setAddress(value,option3);
+                            break;
+                        case 3:
+                            myObj.nextLine();
+                            System.out.println("Enter the phone number : ");
+                            phoneNumber = myObj.nextLine();
+                            faculty.setPhoneNumber(phoneNumber);
+                            break;
+                        case 4:
+                            myObj.nextLine();
+                            System.out.println("Enter the email id : ");
+                            email= myObj.nextLine();
+                            faculty.setEmail(email);
+                            break;
+                        case 5:
+                            System.out.println("Enter the id : ");
+                            id = myObj.nextInt();
+                            faculty.setId(id);
+                            break;
+                        case 6:
+                            myObj.nextLine();
+                            System.out.println("Enter the dept : ");
+                            dept = myObj.nextLine();
+                            faculty.setDepartment(dept);
+                            break;
+                        case 7:
+                            myObj.nextLine();
+                            System.out.println("Enter the specialization : ");
+                            specialization = myObj.nextLine();
+                            faculty.setSpecialization(specialization);
+                            break;
+                        case 8:
+                            faculty.displayAttributes();
+                            break;
+                        default :
+                            System.out.println("Not a valid option ...");
+                            break;
+                    }
+                }
+                break;
+            default:
+                System.out.println("Not a valid option, aborting ...");
+                break;
+        }
+
+    }
+}
